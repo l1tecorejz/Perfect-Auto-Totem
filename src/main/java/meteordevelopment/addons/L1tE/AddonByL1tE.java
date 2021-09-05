de99607849d576829300429c3e16e0a5eab182da
l1tecorejz <90119242+l1tecorejz@users.noreply.github.com>
2021-09-05 04:19:42 +0200

    1

---
+++ src/main/java/meteordevelopment/addons/L1tE/AddonByL1tE.java
@@ -0,0 +1,28 @@
+package meteordevelopment.addons.L1tE;
+
+import meteordevelopment.addons.L1tE.modules.AutoTotem;
+import meteordevelopment.meteorclient.MeteorAddon;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.item.Items;
+
+import java.lang.invoke.MethodHandles;
+
+public class AddonByL1tE extends MeteorAddon
+{
+    public static final Category CATEGORY = new Category("l1tecorejz'",
+        Items.TOTEM_OF_UNDYING.getDefaultStack());
+
+	@Override public void onInitialize()
+    {
+		// Required when using @EventHandler
+		MeteorClient.EVENT_BUS.registerLambdaFactory("meteordevelopment.addons.L1tE", (lookupInMethod, klass) ->
+            (MethodHandles.Lookup) lookupInMethod.invoke(null, klass, MethodHandles.lookup()));
+
+		// Modules
+		Modules.get().add(new AutoTotem());
+	}
+
+	@Override public void onRegisterCategories() {Modules.registerCategory(CATEGORY);}
+}
