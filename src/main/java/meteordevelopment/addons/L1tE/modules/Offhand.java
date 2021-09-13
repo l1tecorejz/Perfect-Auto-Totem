56c5cf1424cff0d8b11e6fb97a71f63eb9f3497c
l1tecorejz <90119242+l1tecorejz@users.noreply.github.com>
2021-09-13 09:59:35 +0200

    another b1g update

--- src/main/java/meteordevelopment/addons/L1tE/modules/Offhand.java
+++ src/main/java/meteordevelopment/addons/L1tE/modules/Offhand.java
@@ -1,11 +1,17 @@
 package meteordevelopment.addons.L1tE.modules;
 
 import meteordevelopment.addons.L1tE.BetterMeteorAddon;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import net.minecraft.item.EnchantedGoldenAppleItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.item.SwordItem;
 import net.minecraft.screen.slot.Slot;
 
+import static meteordevelopment.addons.L1tE.utils.inv.Click;
 import static meteordevelopment.addons.L1tE.utils.inv.Swap;
 
 @SuppressWarnings("ConstantConditions")
@@ -26,11 +32,52 @@ public class Offhand extends Module
 
         final Item
             offhand_item = mc.player.getOffHandStack().getItem(),
-            mainhand_item = mc.player.getMainHandStack().getItem();
+            mainhand_item = mc.player.getMainHandStack().getItem(),
+            cursor_item = mc.player.currentScreenHandler.getCursorStack().getItem();
 
-        // TODO: swordfag
+        if (mainhand_item instanceof SwordItem && cfg_swordfag.get())
+        {
+            if (offhand_item instanceof EnchantedGoldenAppleItem) return;
+
+            if (cursor_item instanceof EnchantedGoldenAppleItem)
+            {
+                Click(45);
+                return;
+            }
+
+            int egap_id = -1, gap_id = -1;
+
+            for (Slot slot : mc.player.currentScreenHandler.slots)
+            {
+                Item item = slot.getStack().getItem();
+                if (item instanceof EnchantedGoldenAppleItem)
+                {
+                    egap_id = slot.id;
+                    break;
+                }
+
+                if (gap_id == -1 && item == Items.GOLDEN_APPLE) gap_id = slot.id;
+            }
+
+            if (egap_id == -1)
+            {
+                if (cursor_item == Items.GOLDEN_APPLE) Click(45);
+                else if (gap_id != -1) Move(gap_id);
+
+                return;
+            }
+
+            Move(egap_id);
+            return;
+        }
 
         if (offhand_item == Items.END_CRYSTAL || mainhand_item == Items.END_CRYSTAL) return;
+
+        if (cursor_item == Items.END_CRYSTAL)
+        {
+            Click(45);
+            return;
+        }
 
         int crystal_id = -1;
 
@@ -45,8 +92,23 @@ public class Offhand extends Module
 
         if (crystal_id == -1) return;
 
-        // TODO: add 1.12.2 mode
-
-        Swap(crystal_id, 40);
+        Move(crystal_id);
     }
+
+    private void Move(int id)
+    {
+        if (AutoTotem.instance.cfg_version.get() == AutoTotem.Versions.one_dot_12) Click(id);
+        else Swap(id, 40);
+    }
+
+    //
+
+    private final SettingGroup sg_general = settings.getDefaultGroup();
+
+    private final Setting<Boolean> cfg_swordfag = sg_general.add(new BoolSetting.Builder()
+        .name("swordfag-helper")
+        .description("Puts gapples in ur offhand if u r using sword.")
+        .defaultValue(false)
+        .build()
+    );
 }
